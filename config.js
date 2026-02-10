bdc960c1ce7e45b19d14ab8a2d8d658551870d7a
hayat00444 <hayat0004444@gmail.com>
2026-02-10 09:47:10 +0530

    dfgdfg

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 export const CONFIG = {
   REDIS_URL:
-    "redis://default:iPuGxsiLbnid04nYb1ZEeflGQA5G0ine@redis-18330.crce179.ap-south-1-1.ec2.cloud.redislabs.com:18330",
+    "redis://default:rP6UmEo5I0K9MmNoQC5wyB1tNYyh295O@redis-10583.crce217.ap-south-1-1.ec2.cloud.redislabs.com:10583",
   MONGO_URI:
     "mongodb+srv://sukd7388_db_user:FyfczxpYHt7VvDlq@rsslot.kfp1p1e.mongodb.net/rsslot",
   PORT: 5000,
