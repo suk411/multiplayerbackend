af49163439c8edf6015c035226eb6c0ff54483dd
hayat00444 <hayat0004444@gmail.com>
2026-02-10 09:48:32 +0530

    trrrdgtd

--- redis.js
+++ redis.js
@@ -2,6 +2,10 @@ import { createClient } from "redis";
 import { CONFIG } from "./config.js";
 
 const redis = createClient({ url: CONFIG.REDIS_URL });
+
+redis.on("error", (err) => console.error("❌ Redis error:", err));
+redis.on("connect", () => console.log("✅ Redis connected"));
+
 await redis.connect();
 
 export default redis;
